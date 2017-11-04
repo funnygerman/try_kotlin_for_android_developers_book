2603bf33c2faa5d482e8ee34e76887c3702630fd
Tim A <na@na.com>
2017-11-04 22:20:41 +0100

    Add an adapter for recyclerview

---
+++ app/src/main/java/com/funnygerman/kotlinweather/ForecastListAdapter.kt
@@ -0,0 +1,21 @@
+package com.funnygerman.kotlinweather
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by timur on 04.11.17.
+ */
+class ForecastListAdapter(private val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(TextView(parent.context))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
