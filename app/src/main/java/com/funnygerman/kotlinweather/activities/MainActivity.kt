c50e774c3aee9017ad842aff1fe1f21dc0e03d25
Tim A <na@na.com>
2017-11-05 14:05:46 +0100

    Add usage of request to retrieve weather data

--- app/src/main/java/com/funnygerman/kotlinweather/activities/MainActivity.kt
+++ app/src/main/java/com/funnygerman/kotlinweather/activities/MainActivity.kt
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.funnygerman.kotlinweather.R
+import com.funnygerman.kotlinweather.Request
 import com.funnygerman.kotlinweather.adapaters.ForecastListAdapter
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,5 +29,13 @@ class MainActivity : AppCompatActivity() {
         val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
     }
 }
