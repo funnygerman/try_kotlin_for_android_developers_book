11763e9296cb546f8a551c8c07cd148c46ed8f5d
Tim A <na@na.com>
2017-11-05 14:03:57 +0100

    Add request class to manage http requests

---
+++ app/src/main/java/com/funnygerman/kotlinweather/Request.kt
@@ -0,0 +1,15 @@
+package com.funnygerman.kotlinweather
+
+import android.util.Log
+import java.net.URL
+
+/**
+ * Created by timur on 05.11.17.
+ */
+class Request(private val url: String) {
+
+    fun run() {
+        val forecastJsonStr = URL(url).readText()
+        Log.d(javaClass.simpleName, forecastJsonStr)
+    }
+}
